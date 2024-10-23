21dd7e13c1d533456ceee087e5422365f213a6c9
dodo5517 <kh9690754@naver.com>
2024-10-23 21:26:21 +0900

    쇼핑몰을 만들어보겠음.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import ogu from './img/ogu.png';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+        <div className="main-bg" style={{ backgroundImage : 'url('+ogu+')'}}></div>
+
+        <Container>
+            <Row>
+                <Col>
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width={"80%"}/>
+                </Col>
+                <Col>
+                    <img src="https://codingapple1.github.io/shop/shoes2.jpg" width={"80%"}/>
+                </Col>
+                <Col>
+                    <img src="https://codingapple1.github.io/shop/shoes3.jpg" width={"80%"}/>
+                </Col>
+            </Row>
+        </Container>
+
     </div>
   );
 }
